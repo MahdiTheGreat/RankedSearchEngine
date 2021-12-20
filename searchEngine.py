eaabf066f634f8113cf6d1285046ee52b89d6445
B1naryB0t <47212121+MahdiTheGreat@users.noreply.github.com>
2021-12-20 17:07:08 +0330

    almost finished however Have to implement options

--- searchEngine.py
+++ searchEngine.py
@@ -2,7 +2,8 @@ from __future__ import unicode_literals
 from hazm import *
 import pandas as pd
 import math
-import bisect
+import heapq
+import numpy as np
 
 
 class UniversalSet(set):
@@ -45,14 +46,26 @@ class Term:
  def __init__(self, id):
 
   self.id = id
+  self.docIds=[]
   self.docTerms = dict()
   self.championsList=[]
 
  def insert(self, docId, position):
   if docId not in self.docTerms.keys():
    self.docTerms[docId] = DocTerm(docId)
-   bisect.insort(self.championsList, docId)
   self.docTerms[docId].insert(position=position)
+
+ def updateLists(self):
+     self.championsList=sorted(self.docTerms.values(),reverse=True)
+     self.docIds=sorted(self.docTerms.keys())
+
+ def updateWeights(self,N):
+     print()
+     for docIds in self.docTerms.keys():
+         doc = self.docTerms[docIds]
+         print()
+         doc.weight = calculateTfIdf(doc.freq, N, len(self.docTerms))
+         print()
 
 
 class Index:
@@ -71,22 +84,20 @@ class Index:
      if term not in self.index.keys():
          self.index[term] = Term(id=term)
          self.termCount += 1
-         self.docs[docId].add(term)
+     self.docs[docId].add(term)
      self.index[term].insert(docId=docId, position=position)
      self.termAxis.append(self.termCount)
      #print()
 
- def calculateTfIdf(self):
-     termIds=self.index.keys()
-     for termId in termIds:
-         term=self.index[termId]
-         docTerms=term.docTerms
-         print()
-         for docIds in docTerms.keys():
-             doc=docTerms[docIds]
-             print()
-             doc.weight=calculateTfIdf(doc.freq,len(self.docs.keys()),len(term.docTerms))
-             print()
+ def updateListsAndDocVectors(self):
+
+     for termId in self.index.keys():
+         self.index[termId].updateWeights(len(self.docs))
+
+     for termId in self.index.keys():
+         self.index[termId].updateLists()
+
+
 
 def calculateTfIdf(termFreq,indexSize,docFreq):
     tf=(1+math.log10(termFreq))
@@ -94,55 +105,27 @@ def calculateTfIdf(termFreq,indexSize,docFreq):
     print()
     return tf*idf
 
-#def rankedQueryResults(index,query,k,r):
-#    score=dict()
-#    length=dict()
-#    terms = preProcessor(query)
-#    queryWeights=dict()
-#
-#    for term in terms:
-#        freq=terms.count(term)
-#        queryWeights[term]=calculateTfIdf(freq,10,1)
-#
-#    for term in terms:
-#        term=index.index[term]
-#        if r<len(term.champions):
-#         docTerms=term.champions[0:r]
-#        else:
-#            docTerms = term.champions
-#        print()
-#        for docTerm in docTerms:
-#            docId=docTerm.id
-#            if docId not in score.keys():
-#                score[docId]=0
-#                length[docId]=0
-#            score[docId]+=docTerm.weight*queryWeights[term]
-#            length[docId]+=math.pow(docTerm.weight,2)
-#
-#    for docId in score.keys():
-#        score[docId]=score[docId]/np.sqrt(length[docId])
-#
-#    score=[(key,score[key]) for key in score.keys()]
-#    score=sorted(score,key=lambda x:x[1])
-#    score=[temp[0] for temp in score]
-#
-#    return score
-
 def rankedQueryResults(index,query,k,r):
-    score=dict()
-    length=dict()
+
     queryTerms = preProcessor(query)
     queryWeights=dict()
     docs=set()
+    score = dict()
+    length = dict()
     print()
 
     for term in queryTerms:
         freq=queryTerms.count(term)
-        queryWeights[term]=calculateTfIdf(freq,10,1)
+        queryWeights[term]=calculateTfIdf(freq,len(index.docs),len(index.index[term].docTerms))
+        #queryWeights[term] = calculateTfIdf(freq, 10, 1)
         if term in index.index.keys():
          champions=index.index[term].championsList
          if r<len(champions):champions=champions[0:r]
-         docs=docs | set(champions)
+         docs=docs | set([champion.docId for champion in champions])
+
+    #queryLength=np.sqrt(np.sum(np.power(list(queryWeights.values()),2)))
+    queryLength=np.linalg.norm(list(queryWeights.values()))
+    #queryLength=1
 
     if len(docs)<k:
         neededDocs=k-len(docs)
@@ -151,25 +134,28 @@ def rankedQueryResults(index,query,k,r):
              if len(docs)>=k:break
              if term in index.index.keys():
                  champions = index.index[term].championsList
-                 if r < len(champions): champions = champions[r+i]
-                 docs = docs | set(champions)
+                 if r+i<len(champions):
+                  champion = champions[r+i]
+                  docs.add(champion.docId)
 
-    docs=list(docs)
-    score=dict()
-    length=dict()
+    docs=heapq.nlargest(k,docs)
+    print()
     for doc in docs:
-        for term in queryTerms :
-            if doc in index.index[term].docTerms.keys():
-             if not doc in score:
-                 score[doc]=0
-                 length[doc]=0
-             weight=index.index[term].docTerms[doc].weight
-             score[doc]+=weight*queryWeights[term]
-             print()
-             length[doc]+=math.pow(weight,2)
+        docTermSet= index.docs[doc]
+        print()
+        for term in docTermSet:
+         if term in queryTerms:
+              if not doc in score:
+                  score[doc]=0
+                  length[doc]=0
+              weight=index.index[term].docTerms[doc].weight
+              score[doc]+=weight*queryWeights[term]
+              print()
+              length[doc]+=math.pow(weight,2)
 
+    print()
     for docId in score.keys():
-        score[docId]=score[docId]/math.sqrt(length[docId])
+        score[docId]=score[docId]/(math.sqrt(length[docId])*queryLength)
 
     score=[(key,score[key]) for key in score.keys()]
     score=sorted(score,key=lambda x:x[1],reverse=True)
@@ -241,7 +227,6 @@ docsDatabaseFileName="IR1_7k_news100.xlsx"
 maxSize=8000
 #query="دانشگاه صنعتی امیرکبیر"
 query=input("پرسمان را وارد کنید")
-
 dataSize=maxSize
 haveStopWords=False
 haveStemming=True
@@ -257,11 +242,11 @@ print()
 fileIndex = Index()
 
 indexer(fileIndex,docs,ids,haveStopWords=haveStopWords,haveStemming=haveStemming)
-fileIndex.calculateTfIdf()
+fileIndex.updateListsAndDocVectors()
 
 #results = queryResults(fileIndex.index, query)
 
-results = rankedQueryResults(fileIndex,query,3,4)
+results = rankedQueryResults(fileIndex,query,10,1)
 
 print("تعداد اسناد بازیابی شده برابر است با:")
 print(len(results))
